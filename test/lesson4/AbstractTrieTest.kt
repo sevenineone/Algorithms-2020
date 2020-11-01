881eb463a131b2dced968c73023661445b1c7836
Alexey Dubynin <alexeyka791@gmail.com>
2020-11-01 21:55:03 +0300

    trie own tests done

--- test/lesson4/AbstractTrieTest.kt
+++ test/lesson4/AbstractTrieTest.kt
@@ -109,6 +109,43 @@ abstract class AbstractTrieTest {
             }
             println("All clear!")
         }
+        ///////////////////////////////////////////////////////
+        implementationTest { create().iterator().hasNext() }
+        implementationTest { create().iterator().next() }
+        val controlSet = mutableSetOf<String>()
+        controlSet.addAll(arrayOf("a", "afd", "adc", "b", "bbg", "bbggf", "h"))
+        println("Control set: $controlSet")
+        val trieSet = create()
+        assertFalse(
+            trieSet.iterator().hasNext(),
+            "Iterator of an empty set should not have any next elements."
+        )
+        for (element in controlSet) {
+            trieSet += element
+        }
+        val iterator1 = trieSet.iterator()
+        val iterator2 = trieSet.iterator()
+        println("Checking if calling hasNext() changes the state of the iterator...")
+        while (iterator1.hasNext()) {
+            assertEquals(
+                iterator2.next(), iterator1.next(),
+                "Calling TrieIterator.hasNext() changes the state of the iterator."
+            )
+        }
+        val trieIter = trieSet.iterator()
+        println("Checking if the iterator traverses the entire set...")
+        while (trieIter.hasNext()) {
+            controlSet.remove(trieIter.next())
+        }
+        assertTrue(
+            controlSet.isEmpty(),
+            "TrieIterator doesn't traverse the entire set."
+        )
+        assertFailsWith<IllegalStateException>("Something was supposedly returned after the elements ended") {
+            trieIter.next()
+        }
+        println("All clear!")
+
     }
 
     protected fun doIteratorRemoveTest() {
@@ -170,6 +207,57 @@ abstract class AbstractTrieTest {
             }
             println("All clear!")
         }
+        //////////////////////////////////////
+        implementationTest { create().iterator().remove() }
+        val controlSet = mutableSetOf<String>()
+        controlSet.addAll(arrayOf("a", "afd", "adc", "b", "bbg", "bbggf", "h"))
+        val toRemove = "a"
+
+        println("Initial set: $controlSet")
+        val trieSet = create()
+        for (element in controlSet) {
+            trieSet += element
+        }
+        controlSet.remove(toRemove)
+        println("Control set: $controlSet")
+        println("Removing element \"$toRemove\" from trie set through the iterator...")
+        val iterator = trieSet.iterator()
+        assertFailsWith<IllegalStateException>("Something was supposedly deleted before the iteration started") {
+            iterator.remove()
+        }
+        var counter = trieSet.size
+        while (iterator.hasNext()) {
+            val element = iterator.next()
+            counter--
+            if (element == toRemove) {
+                iterator.remove()
+                assertFailsWith<IllegalStateException>("Trie.remove() was successfully called twice in a row.") {
+                    iterator.remove()
+                }
+            }
+        }
+        assertEquals(
+            0, counter,
+            "TrieIterator.remove() changed iterator position: ${abs(counter)} elements were ${if (counter > 0) "skipped" else "revisited"}."
+        )
+        assertEquals(
+            controlSet.size, trieSet.size,
+            "The size of the set is incorrect: was ${trieSet.size}, should've been ${controlSet.size}."
+        )
+        for (element in controlSet) {
+            assertTrue(
+                trieSet.contains(element),
+                "Trie set doesn't have the element $element from the control set."
+            )
+        }
+        for (element in trieSet) {
+            assertTrue(
+                controlSet.contains(element),
+                "Trie set has the element $element that is not in control set."
+            )
+        }
+        println("All clear!")
+
     }
 
 }
