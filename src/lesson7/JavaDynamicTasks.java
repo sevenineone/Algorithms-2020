6d7223a3dbe93737a1e62b2afbb6440a0ed938dd
Alexey Dubynin <alexeyka791@gmail.com>
2020-12-08 02:35:14 +0200

    Dynamic Tasks started (1st done)

--- src/lesson7/JavaDynamicTasks.java
+++ src/lesson7/JavaDynamicTasks.java
@@ -4,12 +4,14 @@ import kotlin.NotImplementedError;
 
 import java.util.List;
 
+import static java.lang.Math.max;
+
 @SuppressWarnings("unused")
 public class JavaDynamicTasks {
     /**
      * Наибольшая общая подпоследовательность.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например "nematode knowledge" и "empty bottle".
      * Найти их самую длинную общую подпоследовательность -- в примере это "emt ole".
      * Подпоследовательность отличается от подстроки тем, что её символы не обязаны идти подряд
@@ -19,13 +21,34 @@ public class JavaDynamicTasks {
      * При сравнении подстрок, регистр символов *имеет* значение.
      */
     public static String longestCommonSubSequence(String first, String second) {
-        throw new NotImplementedError();
+        int n = first.length() + 1, m = second.length() + 1, i, j;
+        int[][] matrix = new int[n][m];
+        StringBuilder ans = new StringBuilder("");
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < m; j++) {
+                if (first.charAt(i - 1) == second.charAt(j - 1))
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                else
+                    matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1]);
+            }
+        }
+        i = --n;
+        j = --m;
+        while (i > 0 && j > 0) {
+            if (first.charAt(i - 1) == second.charAt(j - 1)) {
+                ans.append(first.charAt(i - 1));
+                --i;
+                --j;
+            } else if (matrix[i - 1][j] == matrix[i][j]) --i;
+            else --j;
+        }
+        return ans.reverse().toString();
     }
 
     /**
      * Наибольшая возрастающая подпоследовательность
      * Сложная
-     *
+     * <p>
      * Дан список целых чисел, например, [2 8 5 9 12 6].
      * Найти в нём самую длинную возрастающую подпоследовательность.
      * Элементы подпоследовательности не обязаны идти подряд,
@@ -41,24 +64,24 @@ public class JavaDynamicTasks {
     /**
      * Самый короткий маршрут на прямоугольном поле.
      * Средняя
-     *
+     * <p>
      * В файле с именем inputName задано прямоугольное поле:
-     *
+     * <p>
      * 0 2 3 2 4 1
      * 1 5 3 4 6 2
      * 2 6 2 5 1 3
      * 1 4 3 2 6 2
      * 4 2 3 1 5 0
-     *
+     * <p>
      * Можно совершать шаги длиной в одну клетку вправо, вниз или по диагонали вправо-вниз.
      * В каждой клетке записано некоторое натуральное число или нуль.
      * Необходимо попасть из верхней левой клетки в правую нижнюю.
      * Вес маршрута вычисляется как сумма чисел со всех посещенных клеток.
      * Необходимо найти маршрут с минимальным весом и вернуть этот минимальный вес.
-     *
+     * <p>
      * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
      */
-    public static int shortestPathOnField(String inputName) {
+    public static int shortestPathOnField(String inputName) { // remember idea to use A*
         throw new NotImplementedError();
     }
 
